e5443080f48c7c6f077accd6f063bdb55c93a9d1
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-04-09 14:50:52 +0200

    ADD: vite start

---
+++ vite-start/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Ciao mondo'
+    }
+  },
+  methods: {
+    sayHello() {
+      console.log('Hello')
+    }
+  },
+  mounted() {
+    console.log('mounted')
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="title">
+      {{ message }}
+
+    </h1>
+    <!-- <button @click="sayHello">
+      Saluta
+    </button> -->
+  </div>
+</template>
+
+<style>
+.title {
+  font-size: 48px;
+  margin-bottom: 20px;
+}
+</style>
