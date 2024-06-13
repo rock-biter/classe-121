9b475474edc38317ad003f190b4a296b36525d25
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-06-13 12:39:22 +0200

    EDIT: without router.js

--- vite-boolpress/src/main.js
+++ vite-boolpress/src/main.js
@@ -1,8 +1,23 @@
 import { createApp } from 'vue'
-import router from './router'
+// import router from './router'
 import './style.css'
 import App from './App.vue'
 
 // views components
+import { createRouter, createWebHistory } from 'vue-router'
+import AppHome from './pages/AppHome.vue'
+import AppAbout from './pages/AppAbout.vue'
+import AppBlog from './pages/AppBlog.vue'
+import AppPost from './pages/AppPost.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', name: 'home', component: AppHome },
+		{ path: '/blog', name: 'blog', component: AppBlog },
+		{ path: '/blog/:slug', name: 'posts.show', component: AppPost },
+		{ path: '/about', name: 'about', component: AppAbout },
+	],
+})
 
 createApp(App).use(router).mount('#app')
